63a3605960a4620c2ca9722758804666aff2a478
Aliaume Morel <aliaume.morel@kolibree.com>
2018-07-24 10:59:42 +0200

    Add ball movement

---
+++ Assets/BallController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+    private Vector2 ballSpeed;
+
+    private void Start()
+    {
+        ballSpeed = Random.insideUnitCircle;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector3 s3 = ballSpeed;
+        transform.position += s3;
+	}
+}
