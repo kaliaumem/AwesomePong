3ab04fdf3f7de3b0853beb4a9f326e18a1096381
Aliaume Morel <aliaume.morel@kolibree.com>
2018-07-25 01:57:18 +0200

    Slow down the ball and make the paddle push the ball

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
     public Vector2 ballSpeed;
+    public float ballSpeedFactor = 0.5f;
 
     private void Start()
     {
@@ -13,6 +14,6 @@ public class BallController : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         Vector3 s3 = ballSpeed;
-        transform.position += s3;
+        transform.position += s3 * ballSpeedFactor;
 	}
 }
