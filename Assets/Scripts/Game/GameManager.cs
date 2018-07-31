249b47b34b17211ed6e379656ef3276f4d851cf4
Unknown <generalflood3@gmail.com>
2018-07-31 19:20:51 +0200

    Modify colors and styles of the paddles and score texts
    Each paddle now has a unique color, and the score texts use these colors.
    Score texts now use TextMesh Pro for improved customisation.

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -5,68 +5,47 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
-    public Text Player1ScoreLabel;
-    public Text Player2ScoreLabel;
+    public TMPro.TextMeshProUGUI Player1ScoreLabel;
+    public TMPro.TextMeshProUGUI Player2ScoreLabel;
 
+    private BallController ball;
     private int Player1Score;
     private int Player2Score;
 
     private void Awake()
     {
         Instance = this;
+        ball = GameObject.FindObjectOfType<BallController>();
     }
 
     private void Update()
     {
-        Player1ScoreLabel.text = Player1Score.ToString();
-        Player2ScoreLabel.text = Player2Score.ToString();
+        Player1ScoreLabel.SetText(Player1Score.ToString());
+        Player2ScoreLabel.SetText(Player2Score.ToString());
     }
 
     public void FlipBallDirection(bool isXAxis)
     {
-        GameObject ball = GameObject.Find("ball");
         if (ball != null)
         {
-            BallController ballController = ball.GetComponent<BallController>();
-            if (ballController != null)
-            {
-                if (isXAxis)
-                {
-                    ballController.ballSpeed.x = -ballController.ballSpeed.x;
-                }
-                else
-                {
-                    ballController.ballSpeed.y = -ballController.ballSpeed.y;
-                }
-            }
+            if (isXAxis)
+                ball.ballSpeed.x = -ball.ballSpeed.x;
+            else
+                ball.ballSpeed.y = -ball.ballSpeed.y;
         }
     }
 
     public void ScorePlayer1()
     {
         Player1Score++;
-        GameObject ball = GameObject.Find("ball");
         if (ball != null)
-        {
-            BallController ballController = ball.GetComponent<BallController>();
-            if (ballController != null)
-            {
-                ballController.PutBackAtCenter();
-            }
-        }
+            ball.PutBackAtCenter();
     }
 
     public void ScorePlayer2()
     {
         Player2Score++;
-        GameObject ball = GameObject.Find("ball");
         if (ball != null)
-        {
-            BallController ballController = ball.GetComponent<BallController>();
-            if (ballController != null)
-            {
-                ballController.PutBackAtCenter();
-            }
-        }
+            ball.PutBackAtCenter();
     }
 }
