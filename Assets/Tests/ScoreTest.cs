db21d9ea326f615765ec7610900073ce60c3677d
Unknown <generalflood3@gmail.com>
2018-08-02 19:07:36 +0200

    Set up score tests
    Checks if the score labels exists
    Test a goal for each player
    Test a goal by bounce for each player

---
+++ Assets/Tests/ScoreTest.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ScoreTest : UITest
+{
+    [UnityTest]
+    public IEnumerator CheckScoreLabels()
+    {
+        yield return LoadScene("GameScene");
+
+        yield return WaitFor(new ObjectAppeared("Canvas/Layout Restrictor/Score/Player 1"));
+        yield return WaitFor(new ObjectAppeared("Canvas/Layout Restrictor/Score/Player 2"));
+        
+        TMPro.TextMeshProUGUI playerScore1 = GameObject.Find("Canvas/Layout Restrictor/Score/Player 1").GetComponent<TMPro.TextMeshProUGUI>();
+        TMPro.TextMeshProUGUI playerScore2 = GameObject.Find("Canvas/Layout Restrictor/Score/Player 2").GetComponent<TMPro.TextMeshProUGUI>();
+
+        Assert.IsNotNull(playerScore1);
+        Assert.IsNotNull(playerScore2);
+
+        Assert.AreEqual(playerScore1.text, "0");
+        Assert.AreEqual(playerScore2.text, "0");
+
+        yield return null;
+    }
+
+    [UnityTest]
+    public IEnumerator TestIncreaseScorePlayer1()
+    {
+        yield return LoadScene("GameScene");
+
+        yield return WaitFor(new ObjectAppeared<BallController>());
+
+        BallController ball = Object.FindObjectOfType<BallController>();
+        ball.ballSpeed = new Vector2(0.75f, 0.25f);
+
+        yield return WaitFor(new BoolCondition(() => ball.moving == true));
+        yield return WaitFor(new BoolCondition(() => ball.moving == false));
+
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 1").GetComponent<TMPro.TextMeshProUGUI>().text, "1");
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 2").GetComponent<TMPro.TextMeshProUGUI>().text, "0");
+    }
+
+    [UnityTest]
+    public IEnumerator TestIncreaseScorePlayer2()
+    {
+        yield return LoadScene("GameScene");
+
+        yield return WaitFor(new ObjectAppeared<BallController>());
+
+        BallController ball = Object.FindObjectOfType<BallController>();
+        ball.ballSpeed = new Vector2(-0.75f, 0.25f);
+
+        yield return WaitFor(new BoolCondition(() => ball.moving == true));
+        yield return WaitFor(new BoolCondition(() => ball.moving == false));
+
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 1").GetComponent<TMPro.TextMeshProUGUI>().text, "0");
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 2").GetComponent<TMPro.TextMeshProUGUI>().text, "1");
+    }
+
+    [UnityTest]
+    public IEnumerator TestBounceAndGoalInFavorOfPlayer1()
+    {
+        yield return LoadScene("GameScene");
+
+        yield return WaitFor(new ObjectAppeared<BallController>());
+
+        BallController ball = Object.FindObjectOfType<BallController>();
+        ball.ballSpeed = new Vector2(-0.5f, 0.05f);
+
+        yield return WaitFor(new BoolCondition(() => ball.moving == true));
+        yield return WaitFor(new BoolCondition(() => ball.moving == false));
+
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 1").GetComponent<TMPro.TextMeshProUGUI>().text, "1");
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 2").GetComponent<TMPro.TextMeshProUGUI>().text, "0");
+    }
+
+    [UnityTest]
+    public IEnumerator TestBounceAndGoalInFavorOfPlayer2()
+    {
+        yield return LoadScene("GameScene");
+
+        yield return WaitFor(new ObjectAppeared<BallController>());
+
+        BallController ball = Object.FindObjectOfType<BallController>();
+        ball.ballSpeed = new Vector2(0.5f, 0.05f);
+
+        yield return WaitFor(new BoolCondition(() => ball.moving == true));
+        yield return WaitFor(new BoolCondition(() => ball.moving == false));
+
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 1").GetComponent<TMPro.TextMeshProUGUI>().text, "0");
+        Assert.AreEqual(GameObject.Find("Canvas/Layout Restrictor/Score/Player 2").GetComponent<TMPro.TextMeshProUGUI>().text, "1");
+    }
+}
